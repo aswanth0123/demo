f477da5cc49789bb5b4a96e204616517cb43f894
aswanth0123 <aswanthsuresh74@gmail.com>
2025-06-19 17:19:05 +0530

    Some changes database changed to postgres sql

--- mypro/mypro/settings.py
+++ mypro/mypro/settings.py
@@ -74,10 +74,20 @@ WSGI_APPLICATION = 'mypro.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mydb',
+        'USER': 'myuser',
+        'PASSWORD': 'mypassword',
+        'HOST': 'db',
+        'PORT': '5433',
     }
 }
 
